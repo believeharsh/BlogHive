fc671d6cb8844f85067cd3cb3a0f1ba87bbfd1ac
Believeharsh <bontinue@gmail.com>
2025-01-17 15:09:12 +0530

    made controllers for routes

--- Server/src/routes/blog.js
+++ Server/src/routes/blog.js
@@ -1,9 +1,8 @@
-import { Router } from "express" ; 
-import multer from 'multer' ; 
-import path from 'path' ; 
-import Blog from "../models/blog.js" ; 
-import Comments from "../models/comments.js" ; 
+import { Router } from "express";
+import multer from 'multer';
+import path from 'path';
 import { asyncHandler } from "../services/asyncHandler.js";
+import { handleAddNewComment, getBlogById, renderAddNewBlog, handleAddNewBlog } from "../controllers/blog.js";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -18,42 +17,10 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 const router = Router();
-router.get("/add-new", asyncHandler(async (req, res) => {
-    res.render("addblog", {
-        user: req.user,
-    })
-})) ; 
 
-router.get("/:id", asyncHandler(async (req, res) => {
-    const blog = await Blog.findById(req.params.id).populate("createdBy");
-    const comments = await Comments.find({ blogId: req.params.id }).populate("createdBy");
-
-    return res.render('blog', {
-        user: req.user,
-        blog: blog,
-        comments,
-    });
-}));
-
-router.post("/comment/:blogId", asyncHandler(async (req, res) => {
-    await Comments.create({
-        content: req.body.content,
-        blogId: req.params.blogId,
-        createdBy: req.user._id,
-    });
-
-    return res.redirect(`/blog/${req.params.blogId}`)
-})) ; 
-
-router.post("/", upload.single("coverImage"), asyncHandler(async (req, res) => {
-    const { title, body } = req.body;
-    const blog = await Blog.create({
-        body,
-        title,
-        createdBy: req.user._id,
-        coverImageURL: `/uploads/${req.file.filename}`,
-    });
-    return res.redirect(`/blog/${blog._id}`);
-}));
+router.get("/add-new", asyncHandler(renderAddNewBlog));
+router.get("/:id", asyncHandler(getBlogById));
+router.post("/comment/:blogId", asyncHandler(handleAddNewComment));
+router.post("/", upload.single("coverImage"), asyncHandler(handleAddNewBlog));
 
 export default router; 
--- Server/src/routes/user.js
+++ Server/src/routes/user.js
@@ -1,43 +1,16 @@
 import { Router } from "express";
-import User from "../models/user.js";
 import { asyncHandler } from "../services/asyncHandler.js";
+import { renderSignIn, renderSignUp, handleSignIn, handleLogout, handleSignUp } from "../controllers/user.js";
 
 const router = Router();
 
 
-router.get("/signin", asyncHandler((req, res) => {
-    return res.render("signin");
-}));
-
-router.get("/signup", asyncHandler((req, res) => {
-    return res.render("signup");
-}));
-
-router.post("/signin", asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const token = await User.matchPassAndGenToken(email, password);
-        return res.cookie("token", token).redirect("/");
-    } catch (error) {
-        return res.render('signin', {
-            error: 'Incorrect email or password'
-        })
-    }
-}));
-
-router.get('/logout', asyncHandler((req, res) => {
-    res.clearCookie("token").redirect("/");
-}));
+router.get("/signin", asyncHandler(renderSignIn));
+router.get("/signup", asyncHandler(renderSignUp));
+router.post("/signin", asyncHandler(handleSignIn));
+router.get('/logout', asyncHandler(handleLogout));
 
 
-router.post("/signup", asyncHandler(async (req, res) => {
-    const { fullName, email, password } = req.body;
-    await User.create({
-        fullName,
-        email,
-        password
-    });
-    return res.redirect("/");
-}));
+router.post("/signup", asyncHandler(handleSignUp));
 
 export default router; 
