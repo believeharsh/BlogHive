a9e7f4b1b8966b9eb2686dafcb18a58d8038820a
Believeharsh <bontinue@gmail.com>
2025-01-26 22:10:39 +0530

    changed the fronted url

--- Server/src/app.js
+++ Server/src/app.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin: ['https://bloghive.vercel.app', 'http://localhost:5173'],
+        origin: ['https://bloghive-blue.vercel.app', 'http://localhost:5173'],
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         credentials: true
     }
