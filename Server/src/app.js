8b2c3460aceb5b25f67b504a00b712d865701e93
Believeharsh <bontinue@gmail.com>
2025-01-28 08:35:15 +0530

    resolved the paths in vercel.json file

--- Server/src/app.js
+++ Server/src/app.js
@@ -9,7 +9,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin: ['https://bloghive-zeta.vercel.app', 'http://localhost:5173'],
+        origin: process.env.FRONTEND_URL,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         credentials: true
     }
